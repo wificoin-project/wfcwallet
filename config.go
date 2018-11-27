eb6126cd56b723a3bacf558963ffdad484046f06
Dengfeng Liu <liudengfeng@kunteng.org.cn>
2018-11-27 14:16:16 +0800

    fixup btcd to wfcd
    Signed-off-by: Dengfeng Liu <liudengfeng@kunteng.org.cn>

--- config.go
+++ config.go
@@ -66,8 +66,8 @@ type config struct {
 	RPCConnect       string                  `short:"c" long:"rpcconnect" description:"Hostname/IP and port of btcd RPC server to connect to (default localhost:8334, testnet: localhost:18334, simnet: localhost:18556)"`
 	CAFile           *cfgutil.ExplicitString `long:"cafile" description:"File containing root certificates to authenticate a TLS connections with btcd"`
 	DisableClientTLS bool                    `long:"noclienttls" description:"Disable TLS for the RPC client -- NOTE: This is only allowed if the RPC client is connecting to localhost"`
-	WfcdUsername     string                  `long:"btcdusername" description:"Username for btcd authentication"`
-	WfcdPassword     string                  `long:"btcdpassword" default-mask:"-" description:"Password for btcd authentication"`
+	WfcdUsername     string                  `long:"wfcdusername" description:"Username for wfcd authentication"`
+	WfcdPassword     string                  `long:"wfcdpassword" default-mask:"-" description:"Password for wfcd authentication"`
 	Proxy            string                  `long:"proxy" description:"Connect via SOCKS5 proxy (eg. 127.0.0.1:9050)"`
 	ProxyUser        string                  `long:"proxyuser" description:"Username for proxy server"`
 	ProxyPass        string                  `long:"proxypass" default-mask:"-" description:"Password for proxy server"`
